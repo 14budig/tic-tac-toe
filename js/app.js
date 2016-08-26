f34c05471c9bc480733a6ac2e5db033404f94aa5
14budig <nbudig@gmail.com>
2016-08-26 09:30:14 -0700

    fixes a bug that sometimes displays 2 victory messages

--- js/app.js
+++ js/app.js
@@ -24,23 +24,23 @@ $(document).ready(function() {
           alert(spaces[0] + " player wins!");
           game = false;
         }
-        if(spaces[1] === spaces[4] && spaces[7] === spaces[1] && !!spaces[1]){
+        else if(spaces[1] === spaces[4] && spaces[7] === spaces[1] && !!spaces[1]){
           alert(spaces[1] + " player wins!");
           game = false;
         }
-        if((spaces[2] === spaces[5] && spaces[2] === spaces[8] && !!spaces[2])||(spaces[2] === spaces[4] && spaces[2] === spaces[6] && !!spaces[2])){
+        else if((spaces[2] === spaces[5] && spaces[2] === spaces[8] && !!spaces[2])||(spaces[2] === spaces[4] && spaces[2] === spaces[6] && !!spaces[2])){
           alert(spaces[2] + " player wins!");
           game = false;
         }
-        if(spaces[3]  === spaces[4] && spaces[3] === spaces [5] && !!spaces[3]){
+        else if(spaces[3]  === spaces[4] && spaces[3] === spaces [5] && !!spaces[3]){
           alert(spaces[3] + " player wins!");
           game = false;
         }
-        if(spaces[6] === spaces[7] && spaces[7] === spaces[8] && !!spaces[6]){
+        else if(spaces[6] === spaces[7] && spaces[7] === spaces[8] && !!spaces[6]){
           alert(spaces[6] + " player wins!");
           game = false;
         }
-        if(spaces[0] && spaces[1] && spaces[2] && spaces[3] && spaces[4] && spaces[5]
+        else if(spaces[0] && spaces[1] && spaces[2] && spaces[3] && spaces[4] && spaces[5]
             && spaces[6] && spaces[7] && spaces[8] && game){
               alert("Draw Game");
               game = false;
